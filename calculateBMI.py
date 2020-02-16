748c38f76fe299b439e20600fa7c3f09903b3dd3
Aine-Kearns <G00387827@gmit.ie>
2020-02-16 13:01:40 +0000

    minor changes

---
+++ calculateBMI.py
@@ -0,0 +1,11 @@
+#Programme to calculate BMI
+
+#height in centimetres
+#weight in kilograms
+
+height = float(input("Enter height in centimeters: "))
+weight = float(input("Enter weight in kilograms: "))
+
+BMI = (weight/height)**2
+
+print("Body Mass Index is BMI")
