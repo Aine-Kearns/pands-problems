920a00814158793bbf237c0a49b8229f860b00ee
Aine-Kearns <G00387827@gmit.ie>
2020-02-16 14:31:18 +0000

    fixing file problems

--- secondstring.py
+++ secondstring.py
@@ -1,1 +1,9 @@
+# homework
+
+s = input("Please enter a sentence:   ")
+
+
+print(s[::-2])
+
+
 
---
+++ variables.py
@@ -0,0 +1,27 @@
+# Aine
+# Looking at variables
+
+x = 914
+y = 3
+z = x + y
+
+print(z)
+
+fac = 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+print(fac)
+
+b = True
+c = False
+
+print(c)
+
+s = "aineckearns@gmail.com"
+
+print(s)
+print(s[0:20:2])
+print(s[0:10])
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(l[2:6:3])
