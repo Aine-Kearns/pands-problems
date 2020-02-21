bb86e652fbe18877c2c7c852115467dc5e5d7df3
Aine.Kearns <Aine.Kearns@ul.ie>
2020-02-21 16:40:59 +0000

    new files

---
+++ labs/Topic04-flow/lab04.05-topthree.py
@@ -0,0 +1,23 @@
+# This program generates 10 random numbers.
+# prints them out, then
+# prints out the top 3
+
+# I will use a list to stroe and 
+# manipulate the numbers
+
+import random
+
+howMany = 10
+topHowMany = 3
+rangeFrom = 0
+rangeTo = 0
+
+numbers = []
+
+for i in range(1,10):
+    numbers.append(random.randint(rangeFrom,rangeTo))
+print ("{} random numbers\t {}".format(howMany,numbers))
+
+topOnes = numbers.copy()
+topOnes.sort(reverse=True)
+print ("The top {} are \t\t {}".format(topHowMany,topOnes[0:topHowMany]))
